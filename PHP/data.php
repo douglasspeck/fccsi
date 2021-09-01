1ecd1acd1b5c568e48bcf5f05291d19a7f6d025c
douglasspeck <speck.empresarial@gmail.com>
2021-09-01 09:31:47 -0300

    Data Updates

--- PHP/data.php
+++ PHP/data.php
@@ -1,5 +1,5 @@
 <?php
-    /* Variables */
+    /* Comitê */
     $comite_alunos = [
         'Amanda Karina',
         'Ana Júlia Herbst',
@@ -22,4 +22,27 @@
     $comite_professores_is_fem = true;
 
     $comite = array_merge($comite_professores,$comite_alunos);
+
+
+
+    /* Atividades Integradoras */
+    
+    $json = file_get_contents('DATA/atividades.json');
+
+    $json_decode = json_decode($json);
+
+    $atividades = array();
+
+    for ($i = 0; $i < count($json_decode->atividades); $i++) {
+        $atividades[$i] = [
+            $json_decode->atividades[$i]->nome,
+            $json_decode->atividades[$i]->categoria,
+            $json_decode->atividades[$i]->data,
+            $json_decode->atividades[$i]->horário,
+            $json_decode->atividades[$i]->descrição,
+            $json_decode->atividades[$i]->responsáveis,
+            $json_decode->atividades[$i]->imagem
+        ];
+    }
+
 ?>
---
+++ atividades.php
@@ -0,0 +1,19 @@
+<html>
+
+    <?php
+        include 'PHP/data.php';
+    
+        $title = 'FCCSI | Atividades Integradoras';
+        $keywords = 'FCCSI, Feira, Feirinha, Salesiano, Itajaí, Atividades Integradoras';
+        $css = ['main', 'fonts', 'alignment', 'footer'];
+        $js = [];
+        include 'PHP/head.php';
+    ?>
+
+    <body>
+        <?php
+            include 'PHP/footer.php';
+        ?>
+    </body>
+
+</html>
