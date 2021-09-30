1bca6e07f69f1ec8dde1253a2a8a916a1159d4c1
douglasspeck <speck.empresarial@gmail.com>
2021-09-29 22:00:36 -0300

    Installed crisp

---
+++ PHP/crisp.php
@@ -0,0 +1,5 @@
+<?php
+
+    echo '<script type="text/javascript">window.$crisp=[];window.CRISP_WEBSITE_ID="39dfbae4-c485-4655-8cc5-64a9a9852b83";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();</script>'
+
+?>
