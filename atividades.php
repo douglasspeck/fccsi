edc997ca4e8caa1d2be3e8bb991b4883ec29296e
douglasspeck <speck.empresarial@gmail.com>
2021-10-13 04:00:31 -0300

    Changed Header

--- atividades.php
+++ atividades.php
@@ -5,7 +5,7 @@
     
         $title = 'FCCSI | Atividades Integradoras';
         $keywords = 'FCCSI, Feira, Feirinha, Salesiano, Itajaí, Atividades Integradoras';
-        $css = ['main', 'fonts', 'alignment', 'header', 'footer'];
+        $css = ['main', 'fonts', 'alignment', 'atividades', 'header', 'footer'];
         $js = [];
         include 'PHP/head.php';
     ?>
@@ -13,7 +13,18 @@
     <body>
         <?php
             include 'PHP/header.php';
-            include 'PHP/under-construction.php';
+        ?>
+
+        <main>
+            <div class="strip">
+                <div class="text-wrapper">
+                    <h1 class="m-title">Inúmeras Atividades</h1>
+                    <h2 class="p-title">Escolha aquelas que mais combinam com você!</h2>
+                </div>
+            </div>
+        </main>
+
+        <?php
             include 'PHP/footer.php';
         ?>
     </body>
