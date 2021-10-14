945b8aca120b81534751bcf1f6ff2230f58fa3e3
douglasspeck <speck.empresarial@gmail.com>
2021-10-14 17:15:55 -0300

    Fixed PHP INI

--- PHP/data.php
+++ PHP/data.php
@@ -37,7 +37,12 @@ function remove_utf8_bom($text)
 
 /* Atividades Integradoras */
     
-    $json = file_get_contents('https://drive.google.com/uc?id=1gvEcdAssEMU3UDtkvIELh4ZsEo-lH3lQ&export=download');
+    $json = '';
+    
+    ini_set("allow_url_fopen", 1);
+    if( ini_get('allow_url_fopen') ) {
+        $json = file_get_contents('https://drive.google.com/uc?id=1gvEcdAssEMU3UDtkvIELh4ZsEo-lH3lQ&export=download');
+    }
 
     $json = remove_utf8_bom($json);
 
