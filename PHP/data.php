1e581bef7cdfc193bc482291f258783474c99931
douglasspeck <speck.empresarial@gmail.com>
2021-10-14 17:23:02 -0300

    Fixed PHP INI

--- PHP/data.php
+++ PHP/data.php
@@ -41,7 +41,9 @@ function file_get_contents_curl($url) {
     curl_setopt($ch, CURLOPT_HEADER, 0);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 3);     
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
 
     $data = curl_exec($ch);
     curl_close($ch);
